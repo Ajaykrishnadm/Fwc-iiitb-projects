066d4213ee1b7bb6d3060a3aab65bd50a5d6ddba
Ajaykrishnadm <116155036+Ajaykrishnadm@users.noreply.github.com>
2025-09-04 21:47:09 +0530

    Create swap arrayval.cpp

---
+++ Tasks/swap arrayval.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+
+int main() {
+  int arr[2] = {10, 20};
+  printf("Before:%d %d\n", arr[0], arr[1]);
+
+swap(&arr[0], &arr[1]);
+ printf("After:%d %d\n", arr[0], arr[1]);
+ return 0;
+}
+// same method in the first question 
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
