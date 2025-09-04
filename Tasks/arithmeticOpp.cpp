d5fb16efdf4d7867b92950ff72b25d6b477b8d2f
Ajaykrishnadm <116155036+Ajaykrishnadm@users.noreply.github.com>
2025-09-04 21:28:11 +0530

    Create arithmeticOpp.cpp

---
+++ Tasks/arithmeticOpp.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void ops(int a, int b, int *c, int *d, int *e, int *f, int *g) {
+    *c = a+b; *d = a-b; *e = a*b; *f = a/b; *g = a%b;
+}
+
+int main() {
+    int x=15, y=6, c,d,e,f,g;
+    ops(x,y,&c,&d,&e,&f,&g);
+    printf("Sum=%d\nDiff=%d\nProd=%d\nQuot=%d\nRem=%d\n", c,d,e,f,g);
+}
